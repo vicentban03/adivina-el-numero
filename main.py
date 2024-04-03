e4efbd68dec179139adebb131491d571e740cee5
vicentban03 <149728541+vicentban03@users.noreply.github.com>
2024-04-02 23:13:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import ramdom
-a = ramdom.randit(1,9)
-x = imput("ingrese un numero del 1 al 9")
-if a == x:
-        print("le atinaste, eres un genio")
-else:
-        print("incorrecto, el numero era", a)
+for i in range(1000):
+        a = ramdom.randit(1,9)
+        x = imput("ingrese un numero del 1 al 9")
+        if a == x:
+                print("le atinaste, eres un genio")
+        else:
+                print("incorrecto, el numero era", a)
 
 
 
